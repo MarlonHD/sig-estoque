5891d515e964bea320e9374cdca2d2c07b2f29d3
MarlonHD <marlonsilva840@gmail.com>
2022-08-28 15:39:23 -0300

    criação do programa principal

---
+++ estoque.c
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///               Projeto Sistema de Controle de Estoques                   ///
+///   Developed by Cleomar Junior and Marlon Silva -- since Aug, 2022       ///
+///////////////////////////////////////////////////////////////////////////////
+///                                Semana 1                                 ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main(void){
+    printf("SIG-Estoque");
+    return 0;
+}
