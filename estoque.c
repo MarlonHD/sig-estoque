7ef3e55d1e5e35afb9241a082d39cacecceba273
MarlonHD <marlonsilva840@gmail.com>
2022-08-31 16:07:24 -0300

    correção de bugs(caracteres)

--- estoque.c
+++ estoque.c
@@ -10,25 +10,29 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
+#include <locale.h>
 
 //Assinatura
 void telaPrincipal(void);
 
 
 int main(void){
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
     telaPrincipal();
     return 0;
 }
 
 //Funções
 void telaPrincipal(void) {
+    setlocale(LC_ALL,"Portuguese_Brazil");
     printf("\n");
     printf("###########################################################################\n");
     printf("###                                                                     ###\n");
     printf("###             Universidade Federal do Rio Grande do Norte             ###\n");
-    printf("###                 Centro de Ensino Superior do Serid%c                 ###\n",162);
-    printf("###               Departamento de Computa%c%co e Tecnologia               ###\n",135,198);
-    printf("###                  Disciplina DCT1106 -- Programa%c%co                  ###\n",135,198);
+    printf("###                 Centro de Ensino Superior do Seridó                 ###\n");
+    printf("###               Departamento de Computação e Tecnologia               ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                  ###\n");
     printf("###               Projeto Sistema de Controle de Estoques               ###\n");
     printf("###   Developed by Cleomar Junior and Marlon Silva -- since Aug, 2022   ###\n");
     printf("###                                                                     ###\n");
@@ -36,10 +40,10 @@ void telaPrincipal(void) {
     printf("###                                                                     ###\n");
     printf("###             = = = Sistema de Controle de Estoques = = =             ###\n");
     printf("###                                                                     ###\n");
-    printf("###            1. M%cdulo Produtos                                       ###\n",162);
-    printf("###            2. M%cdulo Estoque                                        ###\n",162);
-    printf("###            3. M%cdulo Fornecedores                                   ###\n",162);
-    printf("###            4. M%cdulo Relat%crios                                     ###\n",162,162);
+    printf("###            1. Módulo Produtos                                       ###\n");
+    printf("###            2. Módulo Estoque                                        ###\n");
+    printf("###            3. Módulo Fornecedores                                   ###\n");
+    printf("###            4. Módulo Relatórios                                     ###\n");
     printf("###            5. Sobre                                                 ###\n");
     printf("###            0. Sair                                                  ###\n");
     printf("###                                                                     ###\n");
