981255d10f4d7b99a2961bf8616bc58d24cfbfbd
Cleomar-Junior <cl_jr@outlook.com>
2022-09-03 10:30:30 -0300

    Update estoque.c
    Adição da interface dos módulos Produtos e Estoque, e da tela sobre

--- estoque.c
+++ estoque.c
@@ -6,7 +6,7 @@
 ///               Projeto Sistema de Controle de Estoques                   ///
 ///   Developed by Cleomar Junior and Marlon Silva -- since Aug, 2022       ///
 ///////////////////////////////////////////////////////////////////////////////
-///                                Semana 1                                 ///
+///                                Semana 2                                 ///
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
@@ -14,6 +14,9 @@
 
 //Assinatura
 void telaPrincipal(void);
+void tela_sobre(void);
+void moduloEstoque(void);
+void moduloProdutos(void);
 
 
 int main(void){
@@ -45,6 +48,83 @@ void telaPrincipal(void) {
     printf("###            3. Módulo Fornecedores                                   ###\n");
     printf("###            4. Módulo Relatórios                                     ###\n");
     printf("###            5. Sobre                                                 ###\n");
+    printf("###            0. Sair                                                  ###\n");
+    printf("###                                                                     ###\n");
+    printf("###########################################################################\n");
+    printf("\n");
+}
+
+void tela_sobre(void) {
+    printf("\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                 ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                     ###\n");
+    printf("###               Departamento de Computação e Tecnologia                   ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                      ###\n");
+    printf("###               Projeto Sistema de Controle de Estoques                   ###\n");
+    printf("###      Developed by Cleomar Junior and Marlon Silva -- since Aug, 2022    ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###         = = = = = Sistema de Controle de Estoques = = = = =             ###\n");
+    printf("###                                                                         ###\n");
+    printf("###  Pojeto de um programa de controle de estoques, desenvolvido em C e     ###\n");
+    printf("###  durante a disciplina DCT1106 - Programação, do curso de Bacharelado    ###\n");
+    printf("###  em Sistemas de Informação da UFRN. O projeto tem como finalidade a     ###\n");
+    printf("###  criação de um programa com as funcionalidades básicas para facilitar a ###\n");
+    printf("###  administração/controle do inventário de uma empresa.                   ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("\n");
+}
+
+void moduloProdutos(void) {
+    printf("\n");
+    printf("###########################################################################\n");
+    printf("###                                                                     ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte             ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                 ###\n");
+    printf("###               Departamento de Computação e Tecnologia               ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                  ###\n");
+    printf("###               Projeto Sistema de Controle de Estoques               ###\n");
+    printf("###   Developed by Cleomar Junior and Marlon Silva -- since Aug, 2022   ###\n");
+    printf("###                                                                     ###\n");
+    printf("###########################################################################\n");
+    printf("###                                                                     ###\n");
+    printf("###             = = = Sistema de Controle de Estoques = = =             ###\n");
+    printf("###                        = Módulo Produtos =                          ###\n");
+    printf("###                                                                     ###\n");
+    printf("###            1. Cadastrar Produto                                     ###\n");
+    printf("###            2. Procurar Produto                                      ###\n");
+    printf("###            3. Editar Produto                                        ###\n");
+    printf("###            4. Deletar Produto                                       ###\n");
+    printf("###            0. Sair                                                  ###\n");
+    printf("###                                                                     ###\n");
+    printf("###########################################################################\n");
+    printf("\n");
+}
+
+void moduloEstoque(void) {
+    printf("\n");
+    printf("###########################################################################\n");
+    printf("###                                                                     ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte             ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                 ###\n");
+    printf("###               Departamento de Computação e Tecnologia               ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                  ###\n");
+    printf("###               Projeto Sistema de Controle de Estoques               ###\n");
+    printf("###   Developed by Cleomar Junior and Marlon Silva -- since Aug, 2022   ###\n");
+    printf("###                                                                     ###\n");
+    printf("###########################################################################\n");
+    printf("###                                                                     ###\n");
+    printf("###             = = = Sistema de Controle de Estoques = = =             ###\n");
+    printf("###                        = Módulo Estoque =                           ###\n");
+    printf("###                                                                     ###\n");
+    printf("###            1. Cadastrar Estoque                                     ###\n");
+    printf("###            2. Procurar Estoque                                      ###\n");
+    printf("###            3. Editar Estoque                                        ###\n");
+    printf("###            4. Deletar Estoque                                       ###\n");
     printf("###            0. Sair                                                  ###\n");
     printf("###                                                                     ###\n");
     printf("###########################################################################\n");
