fab317bc8fcc740b5e7154dcfc4f5794b36aafe7
MarlonHD <marlonsilva840@gmail.com>
2022-10-10 15:43:32 -0300

    Assinaturas da funções do módulo estoque

---
+++ modulos/estoque.h
@@ -0,0 +1,12 @@
+void moduloEstoque(void);
+
+void telaCadastrarEstoque(void);
+void telaProcurarEstoque(void);
+void telaEditarEstoque(void);
+void telaDeletarEstoque(void);
+
+//Funções módulo estoque
+void cadastrarEstoque(void);
+void editarEstoque(void);
+void procurarEstoque(void);
+void deletarEstoque(void);
