485184303648d6ab8300d61aa97034e344927aff
MarlonHD <marlonsilva840@gmail.com>
2022-10-10 15:36:46 -0300

    Adição das assinaturas do modulo produto

---
+++ modulos/produto.h
@@ -0,0 +1,11 @@
+void moduloProdutos(void);
+
+void telaCatalogarProduto(void);
+void telaBuscarProduto(void);
+void telaEditarProduto(void);
+void telaDeletarProduto(void);
+
+void cadastrarProduto(void);
+void editarProduto(void);
+void procurarProduto(void);
+void deletarProduto(void);
